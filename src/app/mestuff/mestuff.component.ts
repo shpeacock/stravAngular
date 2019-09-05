774d906d7d635e6ff400917d091c68e2f516bcdc
shpeacock <spencerpeacock@gmail.com>
2019-09-04 20:39:10 -0500

    changing the componenet function

--- src/app/mestuff/mestuff.component.ts
+++ src/app/mestuff/mestuff.component.ts
@@ -7,18 +7,25 @@ import {StravaService } from '../strava.service';
   styleUrls: ['./mestuff.component.css']
 })
 export class MestuffComponent implements OnInit {
-  myData: {};
-
+  myData: {
+  };
+  swimTotal: 0;
+  swimDistance: 0;
+  runTotal: 0;
+  bikeTotal: 0;
   constructor(private stravaService: StravaService) { }
 
   ngOnInit() {
+    this.getShit();
   }
 
   getShit() {
-    this.stravaService.getDatData().then((response) => {
-      this.myData = response;
+    this.stravaService.getDatData().then((response: any) => {
+      this.myData = response; 
+      this.swimTotal = response.ytd_swim_totals.distance;
+      this.runTotal = response.ytd_run_totals.distance;
+      this.bikeTotal = response.ytd_bike_totals.distance;
     })
-    .catch(err => {console.log(err)});
   }
 
 }
